b94b0cd6d70631b23dfd68922110fb3230efef95
roman_myakotin <roman.myakotin@hellotickets.com>
2023-07-20 19:00:19 +0300

    Added container

---
+++ container.go
@@ -0,0 +1,63 @@
+package di
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Build iterate by fields of structure environment and for each field search in the container constructor of component.
+// If constructor found, then it calls and return value of constructor writes to the field of structure environment.
+func Resolve(container, environment interface{}) {
+	e := reflect.ValueOf(environment)
+	if e.Kind() != reflect.Ptr {
+		panic("environment must be pointer")
+	}
+	e = e.Elem()
+	if e.Kind() != reflect.Struct {
+		panic("environment must be struct")
+	}
+	for i := 0; i < e.NumField(); i++ {
+		f := e.Field(i)
+		if f.CanSet() {
+			v := findMatchValue(container, f.Type())
+			if v.IsValid() {
+				f.Set(v)
+			}
+		}
+	}
+	CheckContainer(container)
+}
+
+func findMatchValue(c interface{}, typ reflect.Type) reflect.Value {
+	v := reflect.ValueOf(c)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	for i := 0; i < v.NumField(); i++ {
+		f := v.Field(i)
+		t := f.Type()
+		if t.Kind() == reflect.Func {
+			if t.NumIn() == 0 && t.NumOut() == 1 && t.Out(0) == typ {
+				return f.Call(nil)[0]
+			}
+		}
+	}
+	return reflect.Value{}
+
+}
+
+// CheckContainer checks all fields of structure c for nil.
+// It used reflection for this.
+// If field is nil, then panic.
+func CheckContainer(c interface{}) {
+	v := reflect.ValueOf(c)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	for i := 0; i < v.NumField(); i++ {
+		f := v.Field(i)
+		if f.Type().Kind() == reflect.Pointer && f.IsNil() {
+			panic(fmt.Sprintf("field %s is nil", v.Type().Field(i).Name))
+		}
+	}
+}
